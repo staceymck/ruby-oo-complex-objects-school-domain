e1fc071b7d0c32f31415a22e51dd3c47f98fd0a6
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-16 21:38:25 -0600

    commit tree 9257226fa27a87e6134d752bac9d502efaadde00

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= @roster[grade] = []
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade].map do |name|
+            name
+        end
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each do |grade, students_list|
+           sorted_roster[grade] = students_list.sort
+        end
+        sorted_roster
+    end
+end
+
+
+
+
